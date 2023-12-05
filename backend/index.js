cc12dacebe842dc9e77bcca74e77697909e9375b
ziann <cheongziann@gmail.com>
2023-12-05 12:39:22 +0800

    new backend

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+const express = require("express");
+const app = express();
+const port = 3000;
 
 //these are all middleware. facilitate communication or perform specific functions. access to the request and response objects.
 //log info about incoming requests or outgoing responses, check whether user is authenticated before allowing access to certain routes or catch errors
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   console.log("first middleware");
-  next() //go to next middleware
-})
+  next(); //go to next middleware
+});
 
 //shows hello world response from server
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res, next) => {
+  console.log("second middleware");
+  res.send("Hello World!");
+  next();
+});
 
-app.get('/two', (req, res) => {
-    res.send('Hello World 2!')
-  })
+//executed only when /two path invoked in web browser
+app.get("/two", (req, res) => {ß
+  console.log("third middleware");
+  res.send("Hello World 2!");
+});
+
+//executed regardless of path
+app.use((req,res) => {
+  console.log("fourth middleware");
+}) 
 
 //console.log
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-//npx nodemon index.js --> automatically retarts server whenver i make changes to code
+//npx nodemon index.js -- > automatically retarts server whenver i make changes to code
 //node index.js --> stop server manually and run again to see changes in page
